195705133f9bf51e5a4149a5d2fa688e3e583d64
Meet1103 <meetmehta1103@gmail.com>
2019-07-04 10:35:15 -0600

    Updates

---
+++ precache-manifest.34db247235cb26340e8ccd09d59ab13c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa89db789dbb422171882ef2af2e9037",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "405b1d1bc21ced40e9e4",
+    "url": "/news-app/static/css/main.e4379b86.chunk.css"
+  },
+  {
+    "revision": "c80fcfa6fe847e9b4366",
+    "url": "/news-app/static/js/2.b7ea162b.chunk.js"
+  },
+  {
+    "revision": "405b1d1bc21ced40e9e4",
+    "url": "/news-app/static/js/main.67f4eb15.chunk.js"
+  },
+  {
+    "revision": "fa4b01bc436166f464fa",
+    "url": "/news-app/static/js/runtime~main.dc36b5e3.js"
+  }
+]);
